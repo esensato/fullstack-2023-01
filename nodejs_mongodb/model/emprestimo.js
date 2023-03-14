44c31a20110f3ea9d83af48edef6a66f94356447
esensato <esensato@hotmail.com>
2023-03-14 16:39:09 -0300

    14/03/2023

--- nodejs_mongodb/model/emprestimo.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const emprestimoSchema = mongoose.Schema({
-    data: {type: String, required: true},
-    usuario: {type: String, required: true},
-    livro: {type: mongoose.Types.ObjectId, required: true, ref: 'Livro'}
-});
-
-module.exports = mongoose.model('Emprestimo', emprestimoSchema);
